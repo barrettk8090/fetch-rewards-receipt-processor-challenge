3e08e7689ef427672db3bdf9e68f9c519c1a8fbb
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-06-12 17:52:25 -0600

    add error messaging

--- fetch_rewards_server.py
+++ fetch_rewards_server.py
@@ -4,23 +4,27 @@ import uuid
 
 app = Flask(__name__)
 
-# Use an in-memory dictionary to store our receipt data for the purposes of this assignment.
+# Use a dictionary to store our receipt data for the purposes of this assignment.
 receipts = {}
 
-# Route for processing of receipt JSON. Returns an ID for the specific receipt.
+# Route for processing of receipt JSON. Returns an ID for the specific receipt. 
+# Also stores original receipt data in receipts dict.
 @app.route('/receipts/process', methods=['POST'])
 def process_receipts():
     receipt = request.get_json()
+    if receipt is None:
+        return jsonify(error="There was an issue processing your receipt. Check to ensure that your receipt JSON matches the requirements."), 400
     receipt_id = str(uuid.uuid4())
-    # Store the original receipt json in association with the id
     receipts[receipt_id] = receipt
     return jsonify(id=receipt_id), 201
 
 
-# Route for getting points based on ID
+# Route for getting points based on ID.
 @app.route('/receipts/<string:id>/points', methods=['GET'] )
 def get_points(id):
     receipt_by_id = receipts.get(id)
+    if receipt_by_id is None:
+        return jsonify(error="Sorry, a receipt with that ID could not be found."), 404
     points = calculate_total_receipt_points(receipt_by_id)
     return jsonify(points=points), 201
 
